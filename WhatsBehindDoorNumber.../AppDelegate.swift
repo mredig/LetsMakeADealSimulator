d7eec6eba85cf1600863093380c2f1f132e45fd7
Michael Redig <mredig@gmail.com>
2019-06-20 16:26:50 -0500

    Initial Commit

---
+++ WhatsBehindDoorNumber.../AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WhatsBehindDoorNumber...
+//
+//  Created by Michael Redig on 6/20/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+

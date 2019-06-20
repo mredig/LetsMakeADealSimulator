3e8649aae21424431614188014b8e646e85644ec
Michael Redig <mredig@gmail.com>
2019-06-20 18:24:03 -0500

    final simulation

--- WhatsBehindDoorNumber.../ViewController.swift
+++ WhatsBehindDoorNumber.../ViewController.swift
@@ -10,18 +10,58 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+	var printAlong = false
+
 	@IBAction func doButtonAction(_ sender: NSButton) {
 		runSim()
 	}
 
 	func runSim() {
 
+		let total = 1000
 
+		var stubbornWins = 0
+		for _ in 1...total {
+			stubbornWins += playGame(switchingUponReveal: false) ? 1 : 0
+		}
+
+		var switchingWins = 0
+		for _ in 1...total {
+			switchingWins += playGame(switchingUponReveal: true) ? 1 : 0
+		}
+
+		var randomSelectionWins = 0
+		for _ in 1...total {
+			randomSelectionWins += playGame(switchingUponReveal: Bool.random()) ? 1 : 0
+		}
+
+		print("stubborn win rate: \(Double(stubbornWins) / Double(total))")
+		print("switching win rate: \(Double(switchingWins) / Double(total))")
+		print("random choice win rate: \(Double(randomSelectionWins) / Double(total))")
 
 	}
 
-	func generateDoors(switchingUponReveal: Bool) {
-		
+	func playGame(switchingUponReveal: Bool) -> Bool {
+		let selection = Int.random(in: 0..<3)
+
+		var game = Game()
+		game.choose(doorNumber: selection)
+		console("Chose \(selection): ", false)
+
+		console("Revealed \(game.revealABadDoor()): ", false)
+		if switchingUponReveal {
+			game.switchToUnknownDoor()
+			console("Switching Choice to \(game.myChoice): ", false)
+		}
+
+		console("Winning Door was: \(game.winningDoor)", true)
+		return game.didWin
+	}
+
+	func console(_ string: String, _ newLine: Bool) {
+		if printAlong{
+			print(string, terminator: newLine ? "\n" : "")
+		}
 	}
 }
 
@@ -39,6 +79,10 @@ struct Game {
 	var didWin: Bool {
 		guard (0..<3).contains(myChoice) else { return false }
 		return doors[myChoice] == .win
+	}
+	var winningDoor: Int {
+		guard let winning = doors.firstIndex(of: .win) else { fatalError() }
+		return winning
 	}
 
 	init() {
